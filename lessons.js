7ab1eba624a91f7f59cf0ef9ee99c4b69c98e6a9
Arek Herdzik <arekhbm1@gmail.com>
2022-01-11 18:55:13 +0000

    adding lessons file

---
+++ lessons.js
@@ -0,0 +1,51 @@
+[{
+   "id": 1,
+   "topic": "Maths",     
+   "location": "London", 
+   "price": 86
+}, {
+   "id": 2,
+   "topic": "Maths", 
+   "location": "Croydon", 
+   "price": 129
+}, {
+   "id": 3,
+   "topic": "Science", 
+   "location": "London", 
+   "price": 99
+}, {
+   "id": 4,
+   "topic": "Science", 
+   "location": "Croydon", 
+   "price": 111
+}, {
+   "id": 5,
+   "topic": "History", 
+   "location": "London", 
+   "price": 100
+}, {
+   "id": 6,
+   "topic": "History", 
+   "location": "Leeds", 
+   "price": 105
+}, {
+   "id": 7,
+   "topic": "Dance", 
+   "location": "Croydon", 
+   "price": 106
+}, {
+   "id": 8,
+   "topic": "Dance", 
+   "location": "Wembley", 
+   "price": 114
+}, {
+   "id": 9,
+   "topic": "Drama", 
+   "location": "Croydon", 
+   "price": 100
+}, {
+   "id": 10,
+   "topic": "Drama", 
+   "location": "Exeter", 
+   "price": 102
+}]
