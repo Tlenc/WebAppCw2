c520d126271d35d0d35f038bcbca4c8e910c9045
Arek Herdzik <arekhbm1@gmail.com>
2022-01-11 19:00:39 +0000

    creating first GET route

--- server.js
+++ server.js
@@ -10,3 +10,12 @@ app.use(function(req, res, next) {
     console.log("Request date: " + new Date());
     next();
 });
+
+// GET route to grab all information
+app.get("/lessons", function(req, res) {
+    let fs = require('fs');
+    fs.readFile("lessons.js", "utf8", function(err, data){
+       if(err) throw err;
+       res.send(data);
+     });   
+});
