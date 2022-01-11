2c8ab64fd40af2735bd45f15e35979488d86d0ac
Arek Herdzik <arekhbm1@gmail.com>
2022-01-11 19:00:14 +0000

    creating logger middleware

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+
+app.use(express.static('public'))
+
+// the 'logger' middleware
+app.use(function(req, res, next) {
+    console.log("Request IP: " + req.url);
+    console.log("Request date: " + new Date());
+    next();
+});
